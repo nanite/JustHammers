eee05c126fdc50dfbd20de6b612c7ae2b5a22d52
Michael Hillcox <michaelhillcox@gmail.com>
2023-12-24 21:07:48 +0000

    fix: client leak

--- fabric/src/main/java/pro/mikey/justhammers/fabric/HammersFabric.java
+++ fabric/src/main/java/pro/mikey/justhammers/fabric/HammersFabric.java
@@ -10,11 +10,5 @@ public class HammersFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Hammers.init();
-        WorldRenderEvents.BLOCK_OUTLINE.register(this::renderSelectionOutline);
-    }
-
-    private boolean renderSelectionOutline(WorldRenderContext worldRenderContext, WorldRenderContext.BlockOutlineContext blockOutlineContext) {
-        SelectionOutlineRender.render(worldRenderContext.world(), worldRenderContext.camera(), worldRenderContext.tickDelta(), worldRenderContext.matrixStack(), worldRenderContext.consumers(), worldRenderContext.gameRenderer(), worldRenderContext.projectionMatrix(), worldRenderContext.lightmapTextureManager(), worldRenderContext.worldRenderer());
-        return true;
     }
 }
