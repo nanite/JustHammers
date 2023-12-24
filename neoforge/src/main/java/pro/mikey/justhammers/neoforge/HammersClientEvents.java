4b1b25065b17704f0d9b9e771c9c00ecea5b5504
Michael Hillcox <michaelhillcox@gmail.com>
2023-12-24 23:00:21 +0000

    feat: port to 1.20.2 and added neoforge as a dest

---
+++ neoforge/src/main/java/pro/mikey/justhammers/neoforge/HammersClientEvents.java
@@ -0,0 +1,31 @@
+package pro.mikey.justhammers.neoforge;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import pro.mikey.justhammers.client.SelectionOutlineRender;
+
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
+public class HammersClientEvents {
+    @SubscribeEvent
+    public static void onWorldRenderLast(RenderLevelStageEvent event) {
+        if (event.getStage() != RenderLevelStageEvent.Stage.AFTER_CUTOUT_BLOCKS) {
+            return;
+        }
+
+        Minecraft instance = Minecraft.getInstance();
+        SelectionOutlineRender.render(
+                instance.level,
+                event.getCamera(),
+                event.getPartialTick(),
+                event.getPoseStack(),
+                instance.renderBuffers().bufferSource(),
+                instance.gameRenderer,
+                event.getProjectionMatrix(),
+                instance.gameRenderer.lightTexture(),
+                instance.levelRenderer
+         );
+    }
+}
---
+++ neoforge/src/main/java/pro/mikey/justhammers/neoforge/mixin/ServerPlayerGameModeMixinNeoForge.java
@@ -0,0 +1,51 @@
+package pro.mikey.justhammers.neoforge.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerPlayerGameMode;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import pro.mikey.justhammers.HammerItem;
+
+/**
+ * Forge and fabric seem to have different source sets so this is required... Fun
+ */
+@Debug(export = true)
+@Mixin(ServerPlayerGameMode.class)
+public class ServerPlayerGameModeMixinNeoForge {
+    @Shadow @Final protected ServerPlayer player;
+
+    @Shadow protected ServerLevel level;
+
+    /**
+     * This is used when we're in creative mode, and we're trying to mine a block
+     */
+    @Inject(method = "destroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayerGameMode;removeBlock(Lnet/minecraft/core/BlockPos;Z)Z", ordinal = 0, shift = At.Shift.BEFORE))
+    public void justhammers$beforeRemoveBlock(BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+        this.hammerMineBlock(blockPos);
+    }
+
+    /**
+     * This is used when we're in survival mode, and we're trying to mine a block
+     */
+    @Inject(method = "destroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;mineBlock(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;)V", ordinal = 0, shift = At.Shift.BEFORE))
+    public void justhammers$beforeMineBlock(BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+        this.hammerMineBlock(blockPos);
+    }
+
+    private void hammerMineBlock(BlockPos blockPos) {
+        ItemStack itemStack = this.player.getMainHandItem();
+        if (itemStack.getItem() instanceof HammerItem hammerItem) {
+            BlockState blockState = this.level.getBlockState(blockPos);
+            hammerItem.causeAoe(this.level, blockPos, blockState, itemStack, this.player);
+        }
+    }
+}
