de7197204160bccaa424ad2b75d816250c6374cd
Michael Hillcox <michaelhillcox@gmail.com>
2023-12-24 16:58:06 +0000

    fix: #14 and #16

--- fabric/src/main/java/pro/mikey/justhammers/fabric/mixin/ServerPlayerGameModeMixinFabric.java
+++ fabric/src/main/java/pro/mikey/justhammers/fabric/mixin/ServerPlayerGameModeMixinFabric.java
@@ -1,4 +1,4 @@
-package pro.mikey.justhammers.mixin;
+package pro.mikey.justhammers.fabric.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -6,6 +6,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,18 +15,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import pro.mikey.justhammers.HammerItem;
 
+/**
+ * Forge and fabric seem to have different source sets so this is required... Fun
+ */
+@Debug(export = true)
 @Mixin(ServerPlayerGameMode.class)
-public abstract class ServerPlayerGameModeMixin {
+public class ServerPlayerGameModeMixinFabric {
     @Shadow @Final protected ServerPlayer player;
 
     @Shadow protected ServerLevel level;
 
-    @Inject(method = "destroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayerGameMode;isCreative()Z", ordinal = 0, shift = At.Shift.BEFORE))
-    public void onCreativeRejectOfMineFollowThrough(BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+    /**
+     * This is used when we're in creative mode, and we're trying to mine a block
+     */
+    @Inject(method = "destroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;removeBlock(Lnet/minecraft/core/BlockPos;Z)Z", ordinal = 0, shift = At.Shift.BEFORE))
+    public void justhammers$beforeDestoryBlock(BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
         ItemStack itemStack = this.player.getMainHandItem();
-        if (itemStack.getItem() instanceof HammerItem) {
+        if (itemStack.getItem() instanceof HammerItem hammerItem) {
             BlockState blockState = this.level.getBlockState(blockPos);
-            itemStack.mineBlock(this.level, blockState, blockPos, this.player);
+            hammerItem.causeAoe(this.level, blockPos, blockState, itemStack, this.player);
         }
     }
 }
