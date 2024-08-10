07e94c0ae07df983ba75c6514c3fea60d1972272
Michael Hillcox <michaelhillcox@gmail.com>
2024-08-10 16:48:27 +0100

    fix: fabric crashing on startup #38

--- common/src/main/java/pro/mikey/justhammers/recipe/HammerRecipes.java
+++ common/src/main/java/pro/mikey/justhammers/recipe/HammerRecipes.java
@@ -4,6 +4,7 @@ import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
@@ -13,7 +14,7 @@ public class HammerRecipes {
     public static final DeferredRegister<RecipeType<?>> RECIPE_TYPES = DeferredRegister.create(Hammers.MOD_ID, Registries.RECIPE_TYPE);
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZER = DeferredRegister.create(Hammers.MOD_ID, Registries.RECIPE_SERIALIZER);
 
-    public static final RegistrySupplier<RecipeType<RepairRecipe>> REPAIR_RECIPE = RECIPE_TYPES.register("repair", () -> RecipeType.register("repair"));
+    public static final RegistrySupplier<RecipeType<RepairRecipe>> REPAIR_RECIPE = RECIPE_TYPES.register("repair", () -> RecipeType.register(ResourceLocation.fromNamespaceAndPath(Hammers.MOD_ID, "hammer_repair").toString()));
     public static final RegistrySupplier<RecipeSerializer<RepairRecipe>> REPAIR_RECIPE_SERIALIZER = RECIPE_SERIALIZER.register("repair", () -> new SimpleCraftingRecipeSerializer<>(RepairRecipe::new));
 
     public static void init() {
